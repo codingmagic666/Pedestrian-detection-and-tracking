e01577bf3ff5e273a70829f05ec085448701ecba
codingmagic666 <42923981+codingmagic666@users.noreply.github.com>
2021-04-26 18:13:03 +0800

    Add files via upload

---
+++ YOLOv3-cbam-rfb/Anchor.py
@@ -0,0 +1,54 @@
+# -*- coding=utf-8 -*-
+import glob
+import os
+import sys
+import xml.etree.ElementTree as ET
+import numpy as np
+from kmeans import kmeans, avg_iou
+
+
+ROOT_PATH = '/home/tarzan/cc/pytorchyolo3/data/'
+
+CLUSTERS = 9
+
+SIZE = 640
+
+
+def load_dataset(path):
+    jpegimages = os.path.join(path, 'JPEGImages')
+    if not os.path.exists(jpegimages):
+        print('no JPEGImages folders, program abort')
+        sys.exit(0)
+    labels_txt = os.path.join(path, 'labels')
+    if not os.path.exists(labels_txt):
+        print('no labels folders, program abort')
+        sys.exit(0)
+
+    label_file = os.listdir(labels_txt)
+    print('label count: {}'.format(len(label_file)))
+    dataset = []
+
+    for label in label_file:
+        with open(os.path.join(labels_txt, label), 'r') as f:
+            txt_content = f.readlines()
+
+        for line in txt_content:
+            line_split = line.split(' ')
+            roi_with = float(line_split[len(line_split)-2])
+            roi_height = float(line_split[len(line_split)-1])
+            if roi_with == 0 or roi_height == 0:
+                continue
+            dataset.append([roi_with, roi_height])
+            # print([roi_with, roi_height])
+
+    return np.array(dataset)
+
+data = load_dataset(ROOT_PATH)
+out = kmeans(data, k=CLUSTERS)
+
+print(out)
+print("Accuracy: {:.2f}%".format(avg_iou(data, out) * 100))
+print("Boxes:\n {}-{}".format(out[:, 0] * SIZE, out[:, 1] * SIZE))
+
+ratios = np.around(out[:, 0] / out[:, 1], decimals=2).tolist()
+print("Ratios:\n {}".format(sorted(ratios)))
